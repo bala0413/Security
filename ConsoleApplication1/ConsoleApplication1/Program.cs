aead6660e625bb1a1bffeb541c7ccffd01122d8d
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-16 11:51:08 +0530

    write main code

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi...Hello");
+            Console.ReadLine();
         }
     }
 }
